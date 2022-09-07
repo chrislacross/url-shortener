425621a64b3a79eb2eb99a0095262399a5a996d7
chrislacross <chrislacross@me.com>
2022-09-07 18:41:40 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const app = express()
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req,res) => {
+  try{
+      const short_url = await ShortUrl.findOne({short: req.params.shortUrl })
+
+      if(short_url == null) return res.sendStatus(404)
+  
+      short_url.clicks++
+      short_url.save()
+  
+      res.redirect(short_url.full)
+  } catch (err) {
+      console.log(err)
+  }
+})
+
+app.listen(process.env.PORT || 3000);
